880962082beea14a6abed697ac115993ffe2895d
aliraja-dev <aliraja.en@gmail.com>
2022-03-01 11:14:27 -0500

    Template Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-learning-project';
+  defaultSecretQs : string = "teacher";
+  qsAnswer:string = "";
+  genders: string[]= ['male', 'female', 'binary'];
+  @ViewChild('f') form: NgForm;
+  formSubmitted: boolean=false;
+  userData={
+    gender:"",
+    userData:{
+      username:"",
+      email:""
+    },
+    secretQs:{
+      secret:'',
+      answer:''
+    }
+
+  }
+  // onSubmit(form:NgForm){
+  //   console.log( form);
+  // }
+  onSubmit(){
+    console.log(this.form);
+    this.formSubmitted=true;
+    this.userData.gender= this.form.value.gender;
+    this.userData.userData.username= this.form.value.userData.username;
+    this.userData.userData.email= this.form.value.userData.email;
+    this.userData.secretQs.secret= this.form.value.secretQs.secret;
+    this.userData.secretQs.answer= this.form.value.secretQs.answer
+    this.form.reset();
+  }
+
+
+  onSuggestUserName(){
+    this.form.form.patchValue({
+      userData:{
+        username:"SuperUser"
+      }
+    });
+  }
 }
