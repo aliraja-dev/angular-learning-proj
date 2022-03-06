77544eca926eab9544a14b6fb521571dbf951abc
aliraja-dev <aliraja.en@gmail.com>
2022-03-06 12:20:28 -0500

    mod and child routes added

--- src/app/page-three/page-three-routing.module.ts
+++ src/app/page-three/page-three-routing.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IntroPipesComponent } from './intro-pipes/intro-pipes.component';
 import { IntroRxjsComponent } from './intro-rxjs/intro-rxjs.component';
+import { ObservablesComponent } from './observables/observables.component';
 import { PageThreeComponent } from './page-three/page-three.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { SubjectsComponent } from './subjects/subjects.component';
 
 const routes: Routes = [
   {path:"", component:PageThreeComponent,
   children:[
     {
       path:'', component: IntroRxjsComponent
+    },
+    {
+      path: 'rxjs', component: RxjsComponent
+    },
+    {
+      path: 'observables', component: ObservablesComponent
+    },
+    {
+      path: 'subjects', component: SubjectsComponent
     },
     {
       path: 'pipes', component: IntroPipesComponent
--- src/app/page-three/page-three.module.ts
+++ src/app/page-three/page-three.module.ts
@@ -5,13 +5,19 @@ import { PageThreeRoutingModule } from './page-three-routing.module';
 import { PageThreeComponent } from './page-three/page-three.component';
 import { IntroRxjsComponent } from './intro-rxjs/intro-rxjs.component';
 import { IntroPipesComponent } from './intro-pipes/intro-pipes.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { SubjectsComponent } from './subjects/subjects.component';
 
 
 @NgModule({
   declarations: [
     PageThreeComponent,
     IntroRxjsComponent,
-    IntroPipesComponent
+    IntroPipesComponent,
+    RxjsComponent,
+    ObservablesComponent,
+    SubjectsComponent
   ],
   imports: [
     CommonModule,
